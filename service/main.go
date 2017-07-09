829b0892f86083a7fa26d63e5bcc1e1e8ba20e61
Vitalii <vetal13@gmail.com>
2017-07-09 12:18:49 +0300

    Stub methods for service

--- service/main.go
+++ service/main.go
@@ -36,6 +36,30 @@ func main() {
 		os.Exit(0)
 	}()
 
+	// Social Tournament Service
+	service := Service{DB: db}
+	service.Initialize()
+
+	service.Take("P6", 200)
+	service.Fund("P1", 300)
+	service.Fund("P2", 300)
+	service.Fund("P3", 300)
+	service.Fund("P4", 500)
+	service.Fund("P5", 1000)
+
+	service.AnnounceTournament(1, 1000)
+	service.JoinTournament(1, "P1", []string{"P2", "P3"})
+	service.JoinTournament(1, "P5", []string{})
+	service.ResultTournament("HZ")
+
+	service.Balance("P1")
+	service.Balance("P2")
+	service.Balance("P3")
+	service.Balance("P4")
+	service.Balance("P5")
+
+	service.Reset()
+
 	// Ozzo-router
 	router := routing.New()
 
--- service/service.go
+++ service/service.go
@@ -1,2 +1,62 @@
 package main
 
+import (
+	"github.com/go-ozzo/ozzo-dbx"
+	"log"
+)
+
+type Service struct {
+	DB *dbx.DB
+}
+
+func (service *Service) Initialize()(error) {
+	log.Println("Initializing service")
+	service.CreatePlayersTable()
+	service.CreateTournamentsTables()
+        return nil
+}
+
+func (service *Service) Reset()(error) {
+        log.Println("Reset DB")
+        return nil
+}
+
+func (service *Service) CreatePlayersTable()(error) {
+	log.Println("Create player table")
+        return nil
+}
+
+func (service *Service) CreateTournamentsTables()(error) {
+	log.Println("Create tournament tables")
+        return nil
+}
+
+func (service *Service) Fund(player string, points int64)(error) {
+	log.Println("Fund:", player, points)
+        return nil
+}
+
+func (service *Service) Take(player string, points int64)(error) {
+	log.Println("Take:", player, points)
+        return nil
+}
+
+func (service *Service) AnnounceTournament(tournament int64, deposit int64)(error) {
+	log.Println("AnnounceTournament:", tournament, deposit)
+        return nil
+}
+
+func (service *Service) JoinTournament(tournament int64, player string, backers []string)(error) {
+	log.Println("JoinTournament:", tournament, player, backers)
+        return nil
+}
+
+func (service *Service) ResultTournament(result string)(error) {
+	log.Println("ResultTournament:", result)
+        return nil
+}
+
+func (service *Service) Balance(player string)(int64, error) {
+	log.Println("Balance:", player)
+        return 0, nil
+}
